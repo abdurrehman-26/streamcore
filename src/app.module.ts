9d8c82773f7452d9d5405f3cf9ab09be34f6ab23
abdurehman-26 <ar2522006@gmail.com>
2025-10-31 20:59:02 +0500

    configure database connection and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.DATABASE_URI || 'mongodb://localhost/streamcore',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
