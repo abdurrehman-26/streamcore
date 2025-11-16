577dce969171dd8627d8a2f9379123b8330a33c6
abdurehman-26 <ar2522006@gmail.com>
2025-11-16 22:18:14 +0500

    add ioredis for caching and @nestjs/config for environment variables management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.DATABASE_URI || 'mongodb://localhost/streamcore',
     ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     UsersModule,
   ],
