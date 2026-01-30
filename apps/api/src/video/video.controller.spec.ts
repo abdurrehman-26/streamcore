d6fb1321439852a63f036cb1454fb154f35bd51a
abdurehman-26 <ar2522006@gmail.com>
2026-01-30 16:19:51 +0500

    Add MinIO client mock and implement generateUploadUrl test in video controller

--- apps/api/src/video/video.controller.spec.ts
+++ apps/api/src/video/video.controller.spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VideoController } from './video.controller';
 import { ConfigService } from '@nestjs/config';
 import { Queue } from 'bullmq';
+import * as Minio from 'minio';
 
 describe('videoController', () => {
   let controller: VideoController;
   let videoQueue: jest.Mocked<Pick<Queue, 'add'>>;
+  let minioClient: jest.Mocked<Pick<Minio.Client, 'presignedPutObject'>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,7 +24,9 @@ describe('videoController', () => {
         // Mock MinIO client
         {
           provide: 'MINIO_CLIENT',
-          useValue: {},
+          useValue: {
+            presignedPutObject: jest.fn(),
+          },
         },
 
         // Mock config service
@@ -37,10 +41,32 @@ describe('videoController', () => {
 
     controller = module.get<VideoController>(VideoController);
     videoQueue = module.get('BullQueue_videoProcessing');
+    minioClient = module.get('MINIO_CLIENT');
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('generateUploadUrl', () => {
+    it('should return a presigned URL', async () => {
+      // Mock the presignedPutObject method
+      const mockPresignedUrl = 'http://example.com/presigned-url';
+      minioClient.presignedPutObject = jest
+        .fn()
+        .mockResolvedValue(mockPresignedUrl);
+
+      const result = await controller.generateUploadUrl();
+
+      expect(minioClient.presignedPutObject).toHaveBeenCalledWith(
+        'streamcore',
+        expect.stringMatching(/^raw\/video_\d+\.mp4$/),
+      );
+      expect(result).toEqual({
+        message: 'video upload url generated',
+        url: mockPresignedUrl,
+      });
+    });
   });
 
   describe('handleWebhook', () => {
