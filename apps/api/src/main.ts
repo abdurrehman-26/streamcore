f3f2c956ec5291e059f2ccc9d9940dfe47a556f6
abdurehman-26 <ar2522006@gmail.com>
2026-03-17 08:57:25 +0500

    feat: Enable CORS in the main application bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   app.use(cookieParser());
 
   const config = new DocumentBuilder()
